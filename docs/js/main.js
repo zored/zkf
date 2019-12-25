2f4665b329c60f41706c84a38b912a1b51341b1c
Robert Akhmerov <zored.box@gmail.com>
2019-12-25 14:01:49 +0300

    [master] Add keyboard markup

---
+++ docs/js/main.js
@@ -0,0 +1,49 @@
+document.onload = () => {
+  document.getElementById('content').innerHTML = wrap(
+    '<div class="keybaords"><h2>My keybaords</h2>',
+    '</div>',
+    loadConfig().keybords,
+    (keyboard, name) => wrap(
+      `<div class="keybaord"><h2>Keybaord ${name}<h2>`,
+      '</div>',
+      keyboard.layers,
+      (layer, name) => wrap(
+        `<div class="layer"><h3>Layer ${name}</h3>`,
+        '</div>',
+        layer.keys,
+        (keys, name) => wrap(
+          '<div class="keys-block">`<h4>Keys ${name}</h4>',
+          '</div>',
+          keys,
+          row => wrap(
+            '<div class="keys-row">',
+            '</div>',
+            row,
+            key => wrap(
+              '<span class="key">',
+              '</span>',
+              key,
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+};
+
+function wrap(left, right, inside, map) {
+  if (map !== undefined) {
+    inside = _.map(inside, map);
+  }
+
+  if (Array.isArray(inside)) {
+    inside = inside.join('');
+  }
+  return left + inside + right;
+}
+
+async function loadConfig() {
+  const response = await fetch('https://raw.githubusercontent.com/zored/zkf/master/config.yaml');
+  const text = response.body.text();
+  return jsyaml.load(text);
+}
