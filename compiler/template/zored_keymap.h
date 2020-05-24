74aba7256696e7c6fc34c781da18fff4cfd46e88
Robert Akhmerov <zored.box@gmail.com>
2020-05-25 00:27:23 +0300

    feature-11 Fix headers

--- compiler/template/zored_keymap.h
+++ compiler/template/zored_keymap.h
@@ -1,1 +1,11 @@
-#include QMK_KEYBOARD_H
+#ifndef ZORED_KEYMAP_H
+#define ZORED_KEYMAP_H
+
+typedef enum {
+  OS_MACOS = 1,
+  OS_WINDOWS,
+} operating_systems;
+
+extern operating_systems zored_os;
+
+#endif // ZORED_KEYMAP_H
